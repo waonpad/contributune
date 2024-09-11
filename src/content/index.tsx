d0a4f49121be87bf53492e6f33d23e8b01ce7c9f
waonpad <waonpad@gmail.com>
2024-09-11 19:13:06 +0900

    整理

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,22 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { proxyStore } from "../app/proxy-store";
 import { Content } from "./content";
 
-proxyStore.ready().then(() => {
-  const root = document.body;
+const root = document.body;
 
-  const extRoot = document.createElement("div");
+const extRoot = document.createElement("div");
 
-  root.appendChild(extRoot);
+root.appendChild(extRoot);
 
-  createRoot(extRoot).render(
-    <StrictMode>
-      <Provider store={proxyStore}>
-        <Content />
-      </Provider>
-      ,
-    </StrictMode>,
-  );
-});
+createRoot(extRoot).render(
+  <StrictMode>
+    <Content />,
+  </StrictMode>,
+);
