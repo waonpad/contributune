5eda66014563219af7670c4d9a26437e390fe4ff
waonpad <waonpad@gmail.com>
2024-09-15 04:06:38 +0900

    にゃああ

--- src/utils/canvas.browser.test.tsx
+++ src/utils/canvas.browser.test.tsx
@@ -10,7 +10,7 @@ describe(createRoundRectPath, () => {
   };
 
   test("角丸四角形のパスを作成する", async () => {
-    const { baseElement, unmount } = render(<Canvas />);
+    const { baseElement } = render(<Canvas />);
 
     const canvas = baseElement.querySelector("canvas[data-testid='canvas']") as HTMLCanvasElement;
 
@@ -29,12 +29,10 @@ describe(createRoundRectPath, () => {
     expect(base64).toMatchInlineSnapshot(
       `"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAAAAXNSR0IArs4c6QAAAhZJREFUeJzt3FGOgyAURmGczL7KzmhXZrsy54GkbTpJ4XgvIvp/b/PS6gmiNQPTsixB6vz0PoCRKBagWIBiAYoFKBagWIBiAYoF/Hp90P1+DyHcbrf3P9uJMYYQLpdLCOF6vTb9rpfFLKWUD72vlJL9XL4zxUop9U70qWmylbHmee6d5ZtGydbE2uGA+q9FLxxriFKZey8Wa6BSmW8vEGu4Upljr9pYg5bK5nl2iTVVvlaepqn9STXk8va86ufOdo/IzbicQtXIGn1YZfbBVR5ZBxhWmcOJFGc1lwPdgxijcYI/USz7bbFwGR7mGnRxrpd/z9dt6xRiPR4Py6cfzLlGllHhOesYT1jvLLcsjSxAsQDFAhQLUCxAsQDFAhQLUCxAsQDFAhQLUCxAsQDFAhQLUCxAsQDFAhQLUCxAsQDFAhQLUCxAsQDFAhQLUCxAsQDFAgqx9rBE1ZHxdDSygHPFygvQVyvEGnoxmL/v/ya/87XQlGXFQNUKiyPN8cZY5TnLeJ3vh31K0RI6oOpueIBp3uUUtOwXqH3OGvq26HZl1N8LBr0YO2xVMGivbptgDNer8/YqA/XaxcY92c57eW0R8kGbjQE+29j1ThRijBtsY1f7UFojL9XPy6q32SDx+Qy1za99z1iHd643pUaKBSgWoFiAYgGKBSgWoFiAYgGKBSgWoFiAYgGKBSgWoFiAYgGKBSgW8AcUucEYkZO6qAAAAABJRU5ErkJggg=="`,
     );
-
-    unmount();
   });
 
   test("fillが呼ばれていない", async () => {
-    const { baseElement, unmount } = render(<Canvas />);
+    const { baseElement } = render(<Canvas />);
 
     const canvas = baseElement.querySelector("canvas[data-testid='canvas']") as HTMLCanvasElement;
 
@@ -47,12 +45,10 @@ describe(createRoundRectPath, () => {
     createRoundRectPath({ ctx: canvasCtx, x: 10, y: 10, w: 70, h: 70, r: 20 });
 
     expect(mockFill).not.toHaveBeenCalled();
-
-    unmount();
   });
 
   test("strokeが呼ばれていない", async () => {
-    const { baseElement, unmount } = render(<Canvas />);
+    const { baseElement } = render(<Canvas />);
 
     const canvas = baseElement.querySelector("canvas[data-testid='canvas']") as HTMLCanvasElement;
 
@@ -65,7 +61,5 @@ describe(createRoundRectPath, () => {
     createRoundRectPath({ ctx: canvasCtx, x: 10, y: 10, w: 70, h: 70, r: 20 });
 
     expect(mockStroke).not.toHaveBeenCalled();
-
-    unmount();
   });
 });
